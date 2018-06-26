c3ddb8e6edd988373003dfdaa3e2f11cebf4c9e9
SimonT <simon.turns@capita.co.uk>
2018-06-26 17:03:39 +0100

    updated CEF scenarios

--- src/CEF/ToolStorage.java
+++ src/CEF/ToolStorage.java
@@ -3,7 +3,6 @@ package CEF;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
