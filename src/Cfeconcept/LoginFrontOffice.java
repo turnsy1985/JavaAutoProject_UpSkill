941d9863e52a10f4c36ba6cf58f37ac15fbf85b8
turnsy1985 <37591171+turnsy1985@users.noreply.github.com>
2018-11-29 13:36:15 +0000

    Update LoginFrontOffice.java
    removed credentials

--- src/Cfeconcept/LoginFrontOffice.java
+++ src/Cfeconcept/LoginFrontOffice.java
@@ -18,8 +18,8 @@ public class LoginFrontOffice {
       System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\p10317090\\\\Downloads\\\\chromedriver_win32\\\\chromedriver.exe");
       driver = new ChromeDriver();
       driver.get(baseUrl);        
-      driver.findElement(By.id("userName")).sendKeys("Turnss");
-      driver.findElement(By.id("password")).sendKeys("@Gears1985"); 
+      driver.findElement(By.id("userName")).sendKeys("Xxxxxx");
+      driver.findElement(By.id("password")).sendKeys("Xxxxxx"); 
       driver.findElement(By.id("loginButton")).click();
       driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
       driver.findElement(By.id("f4_listboxProfileTypes_row0_col0")).click();
